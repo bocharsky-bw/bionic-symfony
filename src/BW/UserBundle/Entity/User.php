7790b7f8846106acdec1578f8b59cf23524e565d
victor <bocharsky.bw@gmail.com>
2014-05-21 17:36:58 +0300

    Add role entity

--- src/BW/UserBundle/Entity/User.php
+++ src/BW/UserBundle/Entity/User.php
@@ -4,22 +4,49 @@ namespace BW\UserBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\Common\Collections\ArrayCollection;
 
+/**
+ * Class User
+ * @package BW\UserBundle\Entity
+ */
 class User implements UserInterface, \Serializable
 {
+    /**
+     * @var int The ID
+     */
     private $id;
 
+    /**
+     * @var string The username
+     */
     private $username;
 
+    /**
+     * @var string The password
+     */
     private $password;
 
+    /**
+     * @var string The email
+     */
     private $email;
 
+    /**
+     * @var bool Whether is active
+     */
     private $isActive;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Role", inversedBy="users")
+     *
+     */
+    private $roles;
 
     public function __construct()
     {
         $this->isActive = true;
+        $this->roles = new ArrayCollection();
         // may not be needed, see section on salt below
         // $this->salt = md5(uniqid(null, true));
     }
@@ -90,6 +117,31 @@ class User implements UserInterface, \Serializable
             $this->password,
             // see section on salt below
             // $this->salt
-            ) = unserialize($serialized);
+        ) = unserialize($serialized);
+    }
+
+    public function isAccountNonExpired()
+    {
+        return true;
+    }
+
+    public function isAccountNonLocked()
+    {
+        return true;
+    }
+
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    public function isEnabled()
+    {
+        return $this->isActive;
+    }
+
+    public function getRoles()
+    {
+        return $this->roles->toArray();
     }
 }
